10302ff895dbe7b06b7f429797b703e6781113ef
Fuga Kato <tresener.k@gmail.com>
2021-02-24 01:28:29 +0900

    SIZE/NUM

--- src/ArduinoSPIDWM1001.cpp
+++ src/ArduinoSPIDWM1001.cpp
@@ -45,11 +45,13 @@ DWM1001Error ArduinoSPIDWM1001::write_tlv(
     // at least 35 us wide pulse has to be generated on CS pin to wakeup device
     digitalWrite(pin_cs, LOW); // active low
     delayMicroseconds(50);
+
     SPI.transfer(type);
     SPI.transfer(length);
     for (uint8_t i = 0; i < length; i++) {
         SPI.transfer(value[i]);
     }
+
     digitalWrite(pin_cs, HIGH);
 }
 
@@ -60,15 +62,30 @@ DWM1001Error ArduinoSPIDWM1001::read_tlv(
 {
     if (pin_drdy != 0xff) {
         while (digitalRead(pin_drdy) == LOW)
-            ;
+            delay(1);
     }
-    do {
-        *type = SPI.transfer(0xff);
-        *length = SPI.transfer(0xff);
-    } while (!type);
+
+    digitalWrite(pin_cs, LOW);
+    delayMicroseconds(50);
+
+    uint8_t size, num;
+    while (true)
+        size = SPI.transfer(0xff);
+        num = SPI.transfer(0xff);
+        delay(1);
+        if (size || num)
+            break;
+    }
+    // assert(size == 1 && num == length + 2);
+    // ↑this fails when using backhaul_xfer
+    
+    *type = SPI.transfer(0xff);
+    *length = SPI.transfer(0xff);
     for (uint8_t i = 0; i < length; i++) {
         value[i] = SPI.transfer(0xff);
     }
+
+    digitalWrite(pin_cs, HIGH);
 }
 
 #endif // ifdef ARDUINO
